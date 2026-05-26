c1f23965185e876e6e128e8de7fd78e88eb32397
AbsolKing <130236495+AbsolKing@users.noreply.github.com>
2026-05-26 23:31:42 +0300

    archive: add "Umineko When They Cry Episode 1: Legend of the Golden Witch" to mangaEntries

--- src/data/manga.js
+++ src/data/manga.js
@@ -7,6 +7,17 @@ export const mangaStatuses = [
 
 export const mangaEntries = [
   {
+    title: 'Umineko When They Cry Episode 1: Legend of the Golden Witch',
+    type: 'MANGA',
+    status: 'Completed',
+    statusKey: 'completed',
+    image: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx33749-BDrWQnUkRZmN.png',
+    score: 10,
+    progress: '23 / 23',
+    note: 'GOAT',
+    anilistId: 33749,
+  },
+  {
     title: 'What Do You Call This Trash?',
     type: 'MANGA',
     status: 'Completed',
