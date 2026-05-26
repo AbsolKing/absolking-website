9433a092401e3fdc2f1e5ef607aad71e476385ce
AbsolKing <130236495+AbsolKing@users.noreply.github.com>
2026-05-26 22:56:08 +0300

    archive: add "What Do You Call This Trash?" to mangaEntries

--- src/data/manga.js
+++ src/data/manga.js
@@ -5,4 +5,16 @@ export const mangaStatuses = [
   { value: 'on-hold', label: 'On hold' },
 ]
 
-export const mangaEntries = []
+export const mangaEntries = [
+  {
+    title: 'What Do You Call This Trash?',
+    type: 'MANGA',
+    status: 'Completed',
+    statusKey: 'completed',
+    image: 'https://s4.anilist.co/file/anilistcdn/media/manga/cover/medium/bx150131-ydjyeiIURAbZ.jpg',
+    score: 2,
+    progress: '0 / 32',
+    note: 'Worst manga I\'ve ever read so far. Don\'t even give it a shot.',
+    anilistId: 150131,
+  },
+]
