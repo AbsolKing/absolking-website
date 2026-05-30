dc44600db8d124c5f869d0c0033bd6f5a1338b9f
AbsolKing <130236495+AbsolKing@users.noreply.github.com>
2026-05-30 12:56:39 +0300

    archive: add "Suicide Squad" to movieEntries

--- src/data/movies.js
+++ src/data/movies.js
@@ -7,6 +7,17 @@ export const movieStatuses = [
 
 export const movieEntries = [
   {
+    title: 'Suicide Squad',
+    type: 'Film',
+    status: 'Completed',
+    statusKey: 'completed',
+    image: 'https://image.tmdb.org/t/p/w500/sk3FZgh3sRrmr8vyhaitNobMcfh.jpg',
+    score: 9,
+    note: 'Old guilty pleasure',
+    tmdbId: 297761,
+    tmdbKind: 'movie',
+  },
+  {
     title: 'Kill Bill: The Whole Bloody Affair',
     type: 'Film',
     status: 'Completed',
